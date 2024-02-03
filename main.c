0e827306fe64eda0d59f9f77b8c289a633359549
Floriano Albertini <56269861+IronWyatt@users.noreply.github.com>
2024-02-03 16:36:33 +0100

    Hehe rgb

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: falberti <falberti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: albertini <albertini@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 14:28:35 by falberti          #+#    #+#             */
-/*   Updated: 2024/01/30 18:13:24 by falberti         ###   ########.fr       */
+/*   Updated: 2024/02/03 16:33:51 by albertini        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,27 +29,50 @@ int	handle_input(int keysym, t_mlx_data *data)
 
 void	color_screen(t_mlx_data *data, int color)
 {
-	while (i < SIDE_LEN)
+    int x;
+    int y;
+
+    x = 0;
+    while (x < 800)
+    {
+    y = 0; 
+	while (y < 800)
+    {
+        mlx_pixel_put(data->mlx_ptr, data->win_ptr, x, y, color);
+        y++;
+    }
+        x++;
+    }
 }
+
+int encode_rgb(unsigned char red, unsigned char green, unsigned char blue)
+{
+    return (red << 16 | green << 8 | blue);
+}
+
 
 int	change_bg(int keysum, t_mlx_data *data)
 {
 	if (keysum == KEYCODE_R)
-		color_screen()
+		color_screen(data, encode_rgb(255, 0, 0));
+    else if (keysum == KEYCODE_G)
+        color_screen(data, encode_rgb(0, 255, 0));
+    else if (keysum == KEYCODE_B)
+        color_screen(data, encode_rgb(0, 0, 255));
 	return (0);
 }
 
-// int	change_color(t_mlx_data *data)
-// {
-// 	mlx_string_put(data->mlx_ptr, data->win_ptr, 150, 150, data->color, "Changing color");
-// 	if(data->color == 0xFF0000)
-// 		data->color = 0x0FF000;
-// 	else if (data->color == 0x0FF000)
-// 		data->color = 0x0000FF;
-// 	else
-// 		data->color = 0xFF0000;
-// 	return (0);
-// }
+int	change_color(t_mlx_data *data)
+{
+	mlx_string_put(data->mlx_ptr, data->win_ptr, 150, 150, data->color, "Changing color");
+	if(data->color == 0xFF0000)
+		data->color = 0x0FF000;
+	else if (data->color == 0x0FF000)
+		data->color = 0x0000FF;
+	else
+		data->color = 0xFF0000;
+	return (0);
+}
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -66,7 +89,7 @@ int	main(void)
 	data.mlx_ptr = mlx_init();
 	if (data.mlx_ptr == NULL)
 		return (1);
-	data.win_ptr = mlx_new_window(data.mlx_ptr, 800, 600, "Hello world!");
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 800, 800, "Hello world!");
 	data.color = 0xFF0000;
 	if (data.win_ptr == NULL)
 	{
@@ -74,10 +97,10 @@ int	main(void)
 		free(data.mlx_ptr);
 		return (1);
 	}
-	mlx_key_hook(data.win_ptr, handle_input, &data);
-	mlx_loop_hook(data.mlx_ptr, change_bg, &data);
+	mlx_key_hook(data.win_ptr, change_bg, &data);
+	//mlx_loop_hook(data.mlx_ptr, change_color, &data);
 	//img.img = mlx_new_image(mlx_connection, 800, 600);
-	//img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
+	//img.addr =  (img.img, &img.bits_per_pixel,
 	//								&img.line_length, &img.endian);
 	//my_mlx_pixel_put(&img, 550, 300, 0x00FF0000);
 	//mlx_put_image_to_window(mlx_connection, mlx_win, img.img, 0, 0);
